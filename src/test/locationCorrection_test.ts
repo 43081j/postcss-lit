ec973d692125e87dcaece9ca29562a2b70438dd9
James Garbutt <43081j@users.noreply.github.com>
2021-11-15 23:19:36 +0000

    feat: add typescript support (#3)

--- src/test/locationCorrection_test.ts
+++ src/test/locationCorrection_test.ts
@@ -1,53 +1,10 @@
-import {Document, Root, Rule, Declaration, Node} from 'postcss';
+import {Root, Rule, Declaration} from 'postcss';
 import {assert} from 'chai';
-import {parse} from '../parse.js';
-
-const createTestAst = (source: string): {ast: Document; source: string} => {
-  const ast = parse(source) as Document;
-
-  return {ast, source};
-};
-
-const getSourceForLoc = (source: string, node: Node): string => {
-  const loc = node.source;
-
-  if (!loc || !loc.start || !loc.end) {
-    return '';
-  }
-
-  const lines = source.split(/\r\n|\n/);
-  const result: string[] = [];
-  const startLineIndex = loc.start.line - 1;
-  const endLineIndex = loc.end.line - 1;
-
-  for (let i = startLineIndex; i < loc.end.line; i++) {
-    const line = lines[i];
-    if (line) {
-      let offsetStart = 0;
-      let offsetEnd = line.length;
-
-      if (i === startLineIndex) {
-        offsetStart = loc.start.column - 1;
-      }
-
-      if (i === endLineIndex) {
-        offsetEnd = loc.end.column;
-      }
-
-      result.push(line.substring(offsetStart, offsetEnd));
-    }
-  }
-
-  return result.join('\n');
-};
-
-const getSourceForRange = (source: string, node: Node): string => {
-  if (!node.source || !node.source.start || !node.source.end) {
-    return '';
-  }
-
-  return source.substring(node.source.start.offset, node.source.end.offset + 1);
-};
+import {
+  createTestAst,
+  getSourceForNodeByRange,
+  getSourceForNodeByLoc
+} from './util.js';
 
 describe('locationCorrection', () => {
   it('should translate basic CSS positions', () => {
@@ -60,10 +17,16 @@ describe('locationCorrection', () => {
     const colour = rule.nodes[0] as Declaration;
     assert.equal(colour.type, 'decl');
     assert.equal(rule.type, 'rule');
-    assert.equal(getSourceForLoc(source, rule), '.foo { color: hotpink; }');
-    assert.equal(getSourceForLoc(source, colour), 'color: hotpink;');
-    assert.equal(getSourceForRange(source, rule), '.foo { color: hotpink; }');
-    assert.equal(getSourceForRange(source, colour), 'color: hotpink;');
+    assert.equal(
+      getSourceForNodeByLoc(source, rule),
+      '.foo { color: hotpink; }'
+    );
+    assert.equal(getSourceForNodeByLoc(source, colour), 'color: hotpink;');
+    assert.equal(
+      getSourceForNodeByRange(source, rule),
+      '.foo { color: hotpink; }'
+    );
+    assert.equal(getSourceForNodeByRange(source, colour), 'color: hotpink;');
   });
 
   it('should account for single-line expressions', () => {
@@ -77,15 +40,15 @@ describe('locationCorrection', () => {
     assert.equal(colour.type, 'decl');
     assert.equal(rule.type, 'rule');
     assert.equal(
-      getSourceForLoc(source, rule),
+      getSourceForNodeByLoc(source, rule),
       '.foo { ${expr}color: hotpink; }'
     );
-    assert.equal(getSourceForLoc(source, colour), 'color: hotpink;');
+    assert.equal(getSourceForNodeByLoc(source, colour), 'color: hotpink;');
     assert.equal(
-      getSourceForRange(source, rule),
+      getSourceForNodeByRange(source, rule),
       '.foo { ${expr}color: hotpink; }'
     );
-    assert.equal(getSourceForRange(source, colour), 'color: hotpink;');
+    assert.equal(getSourceForNodeByRange(source, colour), 'color: hotpink;');
   });
 
   it('should account for multiple single-line expressions', () => {
@@ -99,15 +62,21 @@ describe('locationCorrection', () => {
     assert.equal(colour.type, 'decl');
     assert.equal(rule.type, 'rule');
     assert.equal(
-      getSourceForLoc(source, rule),
+      getSourceForNodeByLoc(source, rule),
       '.foo { ${expr}color: ${expr2}hotpink; }'
     );
-    assert.equal(getSourceForLoc(source, colour), 'color: ${expr2}hotpink;');
     assert.equal(
-      getSourceForRange(source, rule),
+      getSourceForNodeByLoc(source, colour),
+      'color: ${expr2}hotpink;'
+    );
+    assert.equal(
+      getSourceForNodeByRange(source, rule),
       '.foo { ${expr}color: ${expr2}hotpink; }'
     );
-    assert.equal(getSourceForRange(source, colour), 'color: ${expr2}hotpink;');
+    assert.equal(
+      getSourceForNodeByRange(source, colour),
+      'color: ${expr2}hotpink;'
+    );
   });
 
   it('should account for multi-line expressions', () => {
@@ -123,19 +92,19 @@ describe('locationCorrection', () => {
     assert.equal(colour.type, 'decl');
     assert.equal(rule.type, 'rule');
     assert.equal(
-      getSourceForLoc(source, rule),
+      getSourceForNodeByLoc(source, rule),
       `.foo { $\{
           expr
         }color: hotpink; }`
     );
-    assert.equal(getSourceForLoc(source, colour), 'color: hotpink;');
+    assert.equal(getSourceForNodeByLoc(source, colour), 'color: hotpink;');
     assert.equal(
-      getSourceForRange(source, rule),
+      getSourceForNodeByRange(source, rule),
       `.foo { $\{
           expr
         }color: hotpink; }`
     );
-    assert.equal(getSourceForRange(source, colour), 'color: hotpink;');
+    assert.equal(getSourceForNodeByRange(source, colour), 'color: hotpink;');
   });
 
   it('should account for multiple mixed-size expressions', () => {
@@ -151,18 +120,41 @@ describe('locationCorrection', () => {
     assert.equal(colour.type, 'decl');
     assert.equal(rule.type, 'rule');
     assert.equal(
-      getSourceForLoc(source, rule),
+      getSourceForNodeByLoc(source, rule),
       `.foo { $\{
           expr
         } $\{expr2}color: hotpink; }`
     );
-    assert.equal(getSourceForLoc(source, colour), 'color: hotpink;');
+    assert.equal(getSourceForNodeByLoc(source, colour), 'color: hotpink;');
     assert.equal(
-      getSourceForRange(source, rule),
+      getSourceForNodeByRange(source, rule),
       `.foo { $\{
           expr
         } $\{expr2}color: hotpink; }`
     );
-    assert.equal(getSourceForRange(source, colour), 'color: hotpink;');
+    assert.equal(getSourceForNodeByRange(source, colour), 'color: hotpink;');
+  });
+
+  it('should account for code before', () => {
+    const {source, ast} = createTestAst(`
+      const foo = bar + baz;
+      css\`
+        .foo { color: hotpink; }
+      \`;
+    `);
+    const rule = (ast.nodes[0] as Root).nodes[0] as Rule;
+    const colour = rule.nodes[0] as Declaration;
+    assert.equal(colour.type, 'decl');
+    assert.equal(rule.type, 'rule');
+    assert.equal(
+      getSourceForNodeByLoc(source, rule),
+      '.foo { color: hotpink; }'
+    );
+    assert.equal(getSourceForNodeByLoc(source, colour), 'color: hotpink;');
+    assert.equal(
+      getSourceForNodeByRange(source, rule),
+      '.foo { color: hotpink; }'
+    );
+    assert.equal(getSourceForNodeByRange(source, colour), 'color: hotpink;');
   });
 });
