079c91d646e4a270d9f2f4e975939f62d11e4f77
Rene Hampölz <voe7c8yk@mask.hampoelz.net>
2025-09-29 18:40:49 +0200

    feat: Add Vite and Rollup support (#67)
    * feat: Add vite/rollup support
    
    * fix: Use correct export
    
    * fix: Add missing semicolon
    
    * docs: Fix rollup example
    
    * chore: remove dev dep
    
    * chore: rename and use default import in docs
    
    * chore: reset lockfile
    
    * chore: bump lilconfig
    
    ---------
    
    Co-authored-by: James Garbutt <43081j@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import {parse} from './parse.js';
 import {stringify} from './stringify.js';
+import {rollupPostCSSLit} from './rollup.js';
 import {stripStyles} from './stripStyles';
 
 export = {
   parse,
   stringify,
+  rollupPostCSSLit,
   tailwindTransform: stripStyles
 };
---
+++ src/rollup.ts
@@ -0,0 +1,60 @@
+import postcss from 'postcss';
+import postcssLoadConfig, {
+  type Result as PostcssConfig
+} from 'postcss-load-config';
+import {type Plugin} from 'rollup';
+
+import {parse} from './parse.js';
+import {stringify} from './stringify.js';
+
+export interface RollupPostCSSLitOptions {
+  /**
+   * Files matching any of these patterns will be processed by PostCSS-Lit.
+   * For more information, see the [`glob` primer](https://github.com/isaacs/node-glob#glob-primer).
+   * @default "**\/*.{js,ts}"
+   */
+  globInclude: string | string[];
+
+  /**
+   * Files matching any of these patterns will be excluded.
+   * For more information, see the [`glob` primer](https://github.com/isaacs/node-glob#glob-primer).
+   * @default ""
+   */
+  globExclude: string | string[];
+}
+
+/**
+ * A Rollup plugin that processes CSS from template
+ * literals inside JS/TS files using PostCSS.
+ *
+ * @param {Partial<RollupPostcssLitOptions>} options Optional plugin options.
+ * @return {Plugin} A Rollup plugin object.
+ */
+export const rollupPostCSSLit = (
+  options: Partial<RollupPostCSSLitOptions> = {}
+): Plugin => {
+  let config: PostcssConfig;
+
+  return {
+    name: 'rollup-plugin-postcss-lit',
+    async buildStart() {
+      config = await postcssLoadConfig({syntax: {parse, stringify}});
+    },
+    transform: {
+      filter: {
+        id: {
+          include: options.globInclude,
+          exclude: options.globExclude
+        }
+      },
+      async handler(code, id) {
+        const result = await postcss(config.plugins).process(code, {
+          ...config.options,
+          from: id,
+          to: id
+        });
+        return {code: result.css};
+      }
+    }
+  };
+};
